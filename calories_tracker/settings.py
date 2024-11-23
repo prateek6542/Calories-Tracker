3d25876195fdbd7c1386479fd379de25499d0520
inspire1805 <inspireuplift1805@gmail.com>
2024-11-23 11:22:10 +0530

    minor changes

--- calories_tracker/settings.py
+++ calories_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_spp&l#h#$&p$z3-l^bn9jd-cm%bspw3=h$sx&vu9d*5upsc&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
